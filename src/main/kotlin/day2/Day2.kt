658e1dd0050615f60769df8ae5fbb045e644922d
Andrew Worley <andrew.worley@johnlewis.co.uk>
2022-12-02 09:45:49 +0000

    Day 2 Part 2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -1,21 +1,64 @@
 package day2
 
+import day2.Outcome.DRAW
+import day2.Outcome.LOSE
+import day2.Outcome.WIN
+import day2.Shape.PAPER
+import day2.Shape.ROCK
+import day2.Shape.SCISSORS
 import util.getInputLines
 
 
 fun main() {
     println(part1(getInputLines(2)))
+    println(part2(getInputLines(2)))
 }
 
 fun part1(input: List<String>): Int {
-    return input.map { parse(it) }.sumOf { winScore(it) + it.you.score }
+    return input.map { parsePart1(it) }.sumOf { winScore(it) + it.you.score }
 }
 
-fun parse(line: String) = line.split(" ").let {
+fun part2(input: List<String>): Int {
+    return input
+        .map { parsePart2(it) }
+        .map {
+            Row(
+                opponent = it.opponent,
+                you = findRequiredShape(it.opponent, it.outcome)
+            )
+        }
+        .sumOf { winScore(it) + it.you.score }
+}
+
+fun parsePart1(line: String) = line.split(" ").let {
     Row(
         you = Shape.from(it[1]),
         opponent = Shape.from(it[0])
     )
+}
+
+fun parsePart2(line: String) = line.split(" ").let {
+    Part2Row(
+        opponent = Shape.from(it[0]),
+        outcome = Outcome.from(it[1])
+    )
+}
+
+fun findRequiredShape(shape: Shape, outcome: Outcome): Shape {
+    return when (outcome) {
+        DRAW -> shape
+        WIN -> when (shape) {
+            SCISSORS -> ROCK
+            ROCK -> PAPER
+            PAPER -> SCISSORS
+        }
+
+        LOSE -> when (shape) {
+            ROCK -> SCISSORS
+            PAPER -> ROCK
+            SCISSORS -> PAPER
+        }
+    }
 }
 
 fun winScore(row: Row): Int {
@@ -26,7 +69,7 @@ fun winScore(row: Row): Int {
     }
 }
 
-enum class Shape(val letters: List<String>, val score: Int) {
+enum class Shape(val part1Letters: List<String>, val score: Int) {
     ROCK(listOf("A", "X"), 1),
     PAPER(listOf("B", "Y"), 2),
     SCISSORS(listOf("C", "Z"), 3);
@@ -39,11 +82,26 @@ enum class Shape(val letters: List<String>, val score: Int) {
     }
 
     companion object {
-        fun from(letter: String) = values().single { it.letters.contains(letter) }
+        fun from(letter: String) = values().single { it.part1Letters.contains(letter) }
+    }
+}
+
+enum class Outcome(val letter: String) {
+    WIN("Z"),
+    LOSE("X"),
+    DRAW("Y");
+
+    companion object {
+        fun from(letter: String) = values().single { it.letter == letter }
     }
 }
 
 data class Row(
     val you: Shape,
     val opponent: Shape
+)
+
+data class Part2Row(
+    val opponent: Shape,
+    val outcome: Outcome
 )
--- src/test/kotlin/day2/Day2KtTest.kt
+++ src/test/kotlin/day2/Day2KtTest.kt
@@ -1,69 +1,104 @@
 package day2
 
+import day2.Outcome.*
 import day2.Shape.PAPER
 import day2.Shape.ROCK
 import day2.Shape.SCISSORS
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import util.getSampleInputLines
 
 internal class Day2KtTest {
-    @Test
-    fun `rock should beat scissors`() {
-        val row = Row(opponent = SCISSORS, you = ROCK)
-        assertThat(winScore(row)).isEqualTo(6)
+
+    @Nested
+    inner class Part1 {
+        @Test
+        fun `rock should beat scissors`() {
+            val row = Row(opponent = SCISSORS, you = ROCK)
+            assertThat(winScore(row)).isEqualTo(6)
+        }
+
+        @Test
+        fun `scissors should beat paper`() {
+            val row = Row(opponent = PAPER, you = SCISSORS)
+            assertThat(winScore(row)).isEqualTo(6)
+        }
+
+        @Test
+        fun `paper should beat rock`() {
+            val row = Row(opponent = ROCK, you = PAPER)
+            assertThat(winScore(row)).isEqualTo(6)
+        }
+
+        @Test
+        fun `scissors should draw with scissors`() {
+            val row = Row(opponent = SCISSORS, you = SCISSORS)
+            assertThat(winScore(row)).isEqualTo(3)
+        }
+
+        @Test
+        fun `paper should draw with paper`() {
+            val row = Row(opponent = PAPER, you = PAPER)
+            assertThat(winScore(row)).isEqualTo(3)
+        }
+
+        @Test
+        fun `rock should draw with rock`() {
+            val row = Row(opponent = ROCK, you = ROCK)
+            assertThat(winScore(row)).isEqualTo(3)
+        }
+
+        @Test
+        fun `scissors should lose to rock`() {
+            val row = Row(opponent = ROCK, you = SCISSORS)
+            assertThat(winScore(row)).isEqualTo(0)
+        }
+
+        @Test
+        fun `paper should lose to scissors`() {
+            val row = Row(opponent = SCISSORS, you = PAPER)
+            assertThat(winScore(row)).isEqualTo(0)
+        }
+
+        @Test
+        fun `rock should lose to paper`() {
+            val row = Row(opponent = PAPER, you = ROCK)
+            assertThat(winScore(row)).isEqualTo(0)
+        }
+
+        @Test
+        fun `should correctly match sample input`() {
+            assertThat(part1(getSampleInputLines(2, 1))).isEqualTo(15)
+        }
     }
 
-    @Test
-    fun `scissors should beat paper`() {
-        val row = Row(opponent = PAPER, you = SCISSORS)
-        assertThat(winScore(row)).isEqualTo(6)
-    }
+    @Nested
+    inner class Part2 {
+        @Test
+        fun `draw should return the same shape`() {
+            assertThat(findRequiredShape(ROCK, DRAW)).isEqualTo(ROCK)
+            assertThat(findRequiredShape(PAPER, DRAW)).isEqualTo(PAPER)
+            assertThat(findRequiredShape(SCISSORS, DRAW)).isEqualTo(SCISSORS)
+        }
 
-    @Test
-    fun `paper should beat rock`() {
-        val row = Row(opponent = ROCK, you = PAPER)
-        assertThat(winScore(row)).isEqualTo(6)
-    }
+        @Test
+        fun `win should return the winning shape`() {
+            assertThat(findRequiredShape(ROCK, WIN)).isEqualTo(PAPER)
+            assertThat(findRequiredShape(PAPER, WIN)).isEqualTo(SCISSORS)
+            assertThat(findRequiredShape(SCISSORS, WIN)).isEqualTo(ROCK)
+        }
 
-    @Test
-    fun `scissors should draw with scissors`() {
-        val row = Row(opponent = SCISSORS, you = SCISSORS)
-        assertThat(winScore(row)).isEqualTo(3)
-    }
+        @Test
+        fun `lose should return the losing shape`() {
+            assertThat(findRequiredShape(ROCK, LOSE)).isEqualTo(SCISSORS)
+            assertThat(findRequiredShape(PAPER, LOSE)).isEqualTo(ROCK)
+            assertThat(findRequiredShape(SCISSORS, LOSE)).isEqualTo(PAPER)
+        }
 
-    @Test
-    fun `paper should draw with paper`() {
-        val row = Row(opponent = PAPER, you = PAPER)
-        assertThat(winScore(row)).isEqualTo(3)
-    }
-
-    @Test
-    fun `rock should draw with rock`() {
-        val row = Row(opponent = ROCK, you = ROCK)
-        assertThat(winScore(row)).isEqualTo(3)
-    }
-
-    @Test
-    fun `scissors should lose to rock`() {
-        val row = Row(opponent = ROCK, you = SCISSORS)
-        assertThat(winScore(row)).isEqualTo(0)
-    }
-
-    @Test
-    fun `paper should lose to scissors`() {
-        val row = Row(opponent = SCISSORS, you = PAPER)
-        assertThat(winScore(row)).isEqualTo(0)
-    }
-
-    @Test
-    fun `rock should lose to paper`() {
-        val row = Row(opponent = PAPER, you = ROCK)
-        assertThat(winScore(row)).isEqualTo(0)
-    }
-
-    @Test
-    fun `should correctly match sample input`() {
-        assertThat(part1(getSampleInputLines(2, 1))).isEqualTo(15)
+        @Test
+        fun `should correctly match sample input`() {
+            assertThat(part2(getSampleInputLines(2, 1))).isEqualTo(12)
+        }
     }
 }
