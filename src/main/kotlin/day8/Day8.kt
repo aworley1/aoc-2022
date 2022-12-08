f1174320ed5d239ca41dab2b71d239f7840550e3
Andrew Worley <andrew.worley@johnlewis.co.uk>
2022-12-08 12:24:32 +0000

    Day 8 Part 2

--- src/main/kotlin/day8/Day8.kt
+++ src/main/kotlin/day8/Day8.kt
@@ -4,6 +4,7 @@ import util.getInputLines
 
 fun main() {
     println(part1(getInputLines(8)))
+    println(part2(getInputLines(8)))
 }
 
 fun part1(input: List<String>): Int {
@@ -24,6 +25,53 @@ fun part1(input: List<String>): Int {
 
     return visibleTrees.size
 }
+
+fun part2(input: List<String>): Int {
+    val trees = parse(input)
+
+    val treesWithTheirViews = parse(input).map {
+        it to listOf(
+            getViewingDistanceDown(trees, it, false),
+            getViewingDistanceDown(trees, it, true),
+            getViewingDistanceRight(trees, it, false),
+            getViewingDistanceRight(trees, it, true)
+        )
+    }
+
+    val treesWithScenicIndex = treesWithTheirViews
+        .map { it.first to it.second[0].size * it.second[1].size* it.second[2].size* it.second[3].size  }
+
+    return treesWithScenicIndex.maxOf { it.second }
+}
+
+fun getViewingDistanceDown(trees: List<Tree>, tree: Tree, reversed: Boolean): List<Tree> {
+    val col = trees
+        .filter { it.col == tree.col }
+        .sortedBy { it.row }
+        .filter { if (reversed) it.row < tree.row else it.row > tree.row }
+        .let { if (reversed) it.reversed() else it }
+
+    val lastVisibleTree = col.firstOrNull { it.height >= tree.height }
+
+    val visibleTrees = lastVisibleTree?.let { col.subList(0, col.indexOf(it) + 1) } ?: col
+
+    return visibleTrees
+}
+
+fun getViewingDistanceRight(trees: List<Tree>, tree: Tree, reversed: Boolean): List<Tree> {
+    val row = trees
+        .filter { it.row == tree.row }
+        .sortedBy { it.col }
+        .filter { if (reversed) it.col < tree.col else it.col > tree.col }
+        .let { if (reversed) it.reversed() else it }
+
+    val lastVisibleTree = row.firstOrNull { it.height >= tree.height }
+
+    val visibleTrees = lastVisibleTree?.let { row.subList(0, row.indexOf(it) + 1) } ?: row
+
+    return visibleTrees
+}
+
 
 fun getVisibleTrees(trees: List<Tree>, rowNum: Int, reversed: Boolean): List<Tree> {
     val row = trees
--- src/test/kotlin/day8/Day8KtTest.kt
+++ src/test/kotlin/day8/Day8KtTest.kt
@@ -7,82 +7,139 @@ import util.getSampleInputLines
 internal class Day8KtTest {
     @Test
     fun `should parse`() {
-        val input = listOf("12","34")
+        val input = listOf("12", "34")
 
         assertThat(parse(input)).containsExactlyInAnyOrder(
-            Tree(0,0, 1), Tree(0,1,2),
-            Tree(1,0, 3), Tree(1,1,4),
+            Tree(0, 0, 1), Tree(0, 1, 2),
+            Tree(1, 0, 3), Tree(1, 1, 4),
         )
     }
 
     @Test
     fun `should count visible trees in a row, forwards`() {
         val trees = listOf(
-            Tree(0,0, 1),
-            Tree(0,1,2),
-            Tree(0,2,2),
-            Tree(0,3,3),
-            Tree(0,3,1),
+            Tree(0, 0, 1),
+            Tree(0, 1, 2),
+            Tree(0, 2, 2),
+            Tree(0, 3, 3),
+            Tree(0, 3, 1),
         )
 
         assertThat(getVisibleTrees(trees, 0, false)).containsExactlyInAnyOrder(
-            Tree(0,0, 1),
-            Tree(0,1,2),
-            Tree(0,3,3),
+            Tree(0, 0, 1),
+            Tree(0, 1, 2),
+            Tree(0, 3, 3),
         )
     }
 
     @Test
     fun `should count visible trees in a row, backwards`() {
         val trees = listOf(
-            Tree(0,0, 1),
-            Tree(0,1,2),
-            Tree(0,2,2),
-            Tree(0,3,3),
-            Tree(0,3,1),
+            Tree(0, 0, 1),
+            Tree(0, 1, 2),
+            Tree(0, 2, 2),
+            Tree(0, 3, 3),
+            Tree(0, 3, 1),
         )
 
         assertThat(getVisibleTrees(trees, 0, true)).containsExactlyInAnyOrder(
-            Tree(0,3,3),
-            Tree(0,3,1),
+            Tree(0, 3, 3),
+            Tree(0, 3, 1),
         )
     }
 
     @Test
     fun `should count visible trees in a column, backwards`() {
         val trees = listOf(
-            Tree(0,0, 1),
-            Tree(1,0,2),
-            Tree(2,0,2),
-            Tree(3,0,3),
-            Tree(4,0,1),
+            Tree(0, 0, 1),
+            Tree(1, 0, 2),
+            Tree(2, 0, 2),
+            Tree(3, 0, 3),
+            Tree(4, 0, 1),
         )
 
         assertThat(getVisibleTreesCol(trees, 0, true)).containsExactlyInAnyOrder(
-            Tree(3,0,3),
-            Tree(4,0,1),
+            Tree(3, 0, 3),
+            Tree(4, 0, 1),
         )
     }
 
     @Test
     fun `should count visible trees in a column, forwards`() {
         val trees = listOf(
-            Tree(0,0, 1),
-            Tree(1,0,2),
-            Tree(2,0,2),
-            Tree(3,0,3),
-            Tree(4,0,1),
+            Tree(0, 0, 1),
+            Tree(1, 0, 2),
+            Tree(2, 0, 2),
+            Tree(3, 0, 3),
+            Tree(4, 0, 1),
         )
 
         assertThat(getVisibleTreesCol(trees, 0, false)).containsExactlyInAnyOrder(
-            Tree(0,0, 1),
-            Tree(1,0,2),
-            Tree(3,0,3),
+            Tree(0, 0, 1),
+            Tree(1, 0, 2),
+            Tree(3, 0, 3),
         )
     }
 
     @Test
+    fun `should find viewing distance down a column`() {
+        val trees = listOf(
+            Tree(0, 0, 1),
+            Tree(1, 0, 2),
+            Tree(2, 0, 2),
+            Tree(3, 0, 3),
+            Tree(4, 0, 1),
+        )
+
+        assertThat(getViewingDistanceDown(trees, Tree(1, 0, 2), false)).isEqualTo(2)
+    }
+
+    @Test
+    fun `should find viewing distance up a column`() {
+        val trees = listOf(
+            Tree(0, 0, 1),
+            Tree(1, 0, 2),
+            Tree(2, 0, 2),
+            Tree(3, 0, 3),
+            Tree(4, 0, 1),
+        )
+
+        assertThat(getViewingDistanceDown(trees, Tree(1, 0, 2), true)).isEqualTo(1)
+    }
+
+    @Test
+    fun `should find viewing distance right across in a row`() {
+        val trees = listOf(
+            Tree(0, 0, 1),
+            Tree(0, 1, 2),
+            Tree(0, 2, 2),
+            Tree(0, 3, 3),
+            Tree(0, 3, 4),
+            Tree(0, 4, 1),
+        )
+
+        assertThat(getViewingDistanceRight(trees, Tree(0, 1, 2),false)).isEqualTo(3)
+    }
+
+    @Test
+    fun `should find viewing distance left across in a row`() {
+        val trees = listOf(
+            Tree(0, 0, 1),
+            Tree(0, 1, 2),
+            Tree(0, 2, 2),
+            Tree(0, 3, 3),
+            Tree(0, 3, 1),
+        )
+
+        assertThat(getViewingDistanceRight(trees, Tree(0, 1, 2),true)).isEqualTo(1)
+    }
+
+    @Test
     fun `should match part 1 sample`() {
-        assertThat(part1(getSampleInputLines(8,1))).isEqualTo(21)
+        assertThat(part1(getSampleInputLines(8, 1))).isEqualTo(21)
+    }
+    @Test
+    fun `should match part 2 sample`() {
+        assertThat(part2(getSampleInputLines(8, 1))).isEqualTo(8)
     }
 }
